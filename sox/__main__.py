8e0e879fc6662aead24c43b62126a027b43d0f6e
Carl Thomé <carlthome@gmail.com>
2016-08-19 09:22:38 +0200

    Start adding streaming

--- sox/__main__.py
+++ sox/__main__.py
@@ -3,8 +3,7 @@ from subprocess import Popen, PIPE
 
 import numpy as np
 
-
-class EffectsChain:
+class Sox:
     def __init__(self):
         self.command = ""
 
@@ -19,24 +18,35 @@ class EffectsChain:
         self.command += ' phaser ' + ' '.join(map(str, [gain_in, gain_out, delay, decay, speed]))
         return self
 
-    def __call__(self, inputsound, outputfile=None):
-
-        if isinstance(inputsound, str):
-            # TODO Allow headerless input files.
-            inputfile = inputsound
-            stdin = None
-        elif isinstance(inputsound, np.ndarray):
-            # TODO Make bit depth and sample rate into parameters.
+    def __call__(self, src=None, dst=np.ndarray):
+        if isinstance(src, np.ndarray):
+            # TODO Don't assume input bit depth and sample rate.
             inputfile = '-t raw -b 32 -r 44100 -e floating-point -'
-            stdin = inputsound.tobytes()
-        elif isinstance(inputsound, list):
+            stdin = src.tobytes()                    
+        elif isinstance(src, str):
+            # TODO Allow headerless input files.
+            inputfile = src
+            stdin = None
+        elif isinstance(src, list):
             # TODO Allow combining files (e.g. take list input args).
             raise ValueError('Combining files not supported yet.')
+        elif src is None:
+            # TODO Allow other audio devices but the default.
+            inputfile = '-d'        
+        else:
+            raise ValueError("Invalid input.")
 
-        if not outputfile:
-            # TODO Make bit depth and sample rate into parameters.
+        if dst is np.ndarray:
+            # TODO Make output bit depth and output sample rate into parameters.
             outputfile = '-t raw -b 32 -r 44100 -e floating-point -'
-
+        elif isinstance(dst, str):
+            outputfile = dst
+        elif dst is None:
+            # TODO Allow other audio devices but the default.
+            outputfile = '-d'
+        else:
+            raise ValueError("Invalid output.")
+            
         # TODO Split command into list before passing it to Popen for OS compatitbility.
         x = 'sox'.split()
         x.extend(inputfile.split())
@@ -61,22 +71,23 @@ p = lr.util.example_audio_file()
 y, sr = lr.load(p, sr=None)
 print("Sample rate: ", sr)
 print("Bit depth: ", y.dtype)
-s = EffectsChain().phaser(0.1, 0.1, 0.1, 0.1, 0.1).reverb(True, 50, 50, 100, 100, 25, 10)
+s = Sox().phaser(0.1, 0.1, 0.1, 0.1, 0.1).reverb(True, 50, 50, 100, 100, 25, 10)
 
 # Apply effects.
 output = s(y)
 print(output.shape)
 
-# Load file, and apply effects.
+# Apply effects to file.
 output = s(p)
 print(output.shape)
 
-# Apply effects and output file.
+# Apply effects and save results.
 s(y, 'output.ogg')
 
-# Load file, apply effects and output file.
+# Apply effects to file and save results.
 s(p, 'output.wav')
 
-
+# Stream
+s()
 
 
