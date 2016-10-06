f1140114b7b622edcd0310a0984567e9af4e9927
Carl Thomé <carlthome@gmail.com>
2016-10-06 16:48:34 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,13 +5,25 @@ from setuptools import find_packages, setup
 
 setup(
     name='pysndfx',
-    version='0.0.3',
-    long_description=open('README.rst').read(),
-    license=open('LICENSE').read(),
+    version='0.0.5',
+    long_description='Apply audio effects such as reverb and EQ directly to audio files or NumPy ndarrays.',
+    license='MIT',
     author='Carl Thomé',
     author_email='carlthome@gmail.com',
     url='https://github.com/carlthome/python-audio-effects',
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'librosa'])
+    tests_require=['pytest', 'librosa'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+    ],
+    keywords='audio music sound')
